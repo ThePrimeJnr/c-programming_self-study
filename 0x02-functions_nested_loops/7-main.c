e05bc48b55ac7c9bdcad7188e8187cd2eeadf367
Destiny Saturday <yiradesat@gmail.com>
2023-05-17 22:09:42 +0100

    Write a function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ * 
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date:  17-05-2023
+ */
+
+int main(void)
+{
+	int r;
+
+	print_last_digit(98);
+
+	print_last_digit(0);
+
+	r = print_last_digit(-1024);
+	putchar('0' + r);
+
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: The number to be checked
+ * 
+ * Return: The last digit of the number
+ * 
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date:  17-05-2023
+*/
+
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+
+	if (n < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+
+	printf("%d", last_digit);
+
+	return (last_digit);
+}
