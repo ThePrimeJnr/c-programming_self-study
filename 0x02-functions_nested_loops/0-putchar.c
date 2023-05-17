ffea121472e2e60d58c3a05d3cba7e770068b2d7
Destiny Saturday <yiradesat@gmail.com>
2023-05-17 14:50:03 +0100

    Writes a program that prints '_putchar' followed by a newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints "_putchar" followed by a newline
+ *
+ * Return: Always 0 (Success)
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date:  17-05-2023
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
