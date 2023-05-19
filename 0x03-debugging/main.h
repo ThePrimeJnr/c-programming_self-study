bf5a7111fadb9bd64bfcb0ef18b6273d354da09e
Destiny Saturday <yiradesat@gmail.com>
2023-05-19 17:09:59 +0100

    Added header description

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * Declaration for all functions prototypes and header files
+ *
+ * Author: Destiny Saturday (DestinedCodes)
+ * Date:  19-05-2023
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
